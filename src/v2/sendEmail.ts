897600fc9aa5b0bf85d4aa18f2a23d6963604a8f
+pikari <yasuda.hikaru@gmail.com>
2022-12-23 07:19:43 +0900

    sendMail via template

--- src/v2/sendEmail.ts
+++ src/v2/sendEmail.ts
@@ -15,6 +15,8 @@ const handler: RequestHandler = (req, res, next) => {
     handleSimple(req, res, next);
   } else if (req.body.Content?.Raw) {
     handleRaw(req, res, next);
+  } else if (req.body.Content?.Template) {
+    handleTemplate(req, res, next);
   } else {
     res.status(400).send({ message: 'Bad Request Exception', detail: 'aws-ses-v2-local: Must have either Simple or Raw content. Want to add support for other types of emails? Open a PR!' });
   }
@@ -78,6 +80,86 @@ const handleRaw: RequestHandler = async (req, res) => {
       text: message.text,
     },
     attachments: message.attachments.map((a) => ({ ...a, content: a.content.toString('base64') })),
+    at: Math.floor(new Date().getTime() / 1000),
+  });
+
+  res.status(200).send({ MessageId: messageId });
+};
+
+const parseTemplateData = (json?: string): {[k:string]: unknown}|false => {
+  if (!json) {
+    return {};
+  }
+  try {
+    const data = JSON.parse(json);
+    if (!data || typeof data !== 'object') {
+      return false;
+    }
+    return data;
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.error(e);
+    return false;
+  }
+};
+
+const toString = (value: unknown): string => {
+  switch (value) {
+    case true: return 'true';
+    case false: return 'false';
+    case null: return '';
+    case undefined: return '';
+    default: return `${value}`;
+  }
+};
+
+const compileTemplate = (template: string|undefined, data: {[k:string]: unknown}) => {
+  if (!template) {
+    return template;
+  }
+  return Object.entries(data).reduce((prev, [key, value]) => prev.replace(new RegExp(`\\{\\{${key}}}`, 'g'), toString(value)), template);
+};
+
+const handleTemplate: RequestHandler = async (req, res) => {
+  if (!req.body.Content?.Template?.TemplateName) {
+    res.status(400).send({ message: 'Bad Request Exception', detail: 'aws-ses-v2-local: Template content must have a TemplateName.' });
+    return;
+  }
+
+  if (!req.body.FromEmailAddress) {
+    res.status(400).send({ message: 'Bad Request Exception', detail: 'aws-ses-v2-local: Must have a from email address. Want to add support for other types of emails? Open a PR!' });
+    return;
+  }
+
+  const template = store.templates.get(req.body.Content.Template.TemplateName);
+  if (!template) {
+    res.status(400).send({ message: 'Bad Request Exception', detail: `aws-ses-v2-local: Template not found for "${req.body.Content.Template.TemplateName}"` });
+    return;
+  }
+
+  const data = parseTemplateData(req.body.Content.Template.TemplateData);
+  if (data === false) {
+    res.status(400).send({ message: 'Bad Request Exception', detail: 'aws-ses-v2-local: Invalid template data format.' });
+    return;
+  }
+
+  const messageId = `ses-${Math.floor(Math.random() * 900000000 + 100000000)}`;
+
+  store.emails.push({
+    messageId,
+    from: req.body.FromEmailAddress,
+    replyTo: req.body.ReplyToAddresses ?? [],
+    destination: {
+      to: req.body.Destination?.ToAddresses ?? [],
+      cc: req.body.Destination?.CcAddresses ?? [],
+      bcc: req.body.Destination?.BccAddresses ?? [],
+    },
+    subject: compileTemplate(template.TemplateContent.Subject, data) || '',
+    body: {
+      html: compileTemplate(template.TemplateContent.Html, data),
+      text: compileTemplate(template.TemplateContent.Text, data),
+    },
+    attachments: [],
     at: Math.floor(new Date().getTime() / 1000),
   });
 
